4e7037941c10ae43df8dc12e67decede9f115a9f
mohit-rathi <mohit.rathi@bacancy.com>
2023-02-28 11:34:13 +0530

    Initial commit

---
+++ API/OnlineTest.Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineTest.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(10)]
+        public string Phone { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(256)]
+        public string Password { get; set; }
+    }
+}
---
+++ API/OnlineTest/Data/OnlineTestContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineTest.Models;
+
+namespace OnlineTest.Data
+{
+    public class OnlineTestContext : DbContext
+    {
+        public OnlineTestContext(DbContextOptions<OnlineTestContext> options) : base(options)
+        { }
+
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+    }
+}
